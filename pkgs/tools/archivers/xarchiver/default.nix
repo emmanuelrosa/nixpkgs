c7078ed34fc1b5615503be50facc9191b5877637
Emmanuel Rosa <goaway1000@gmail.com>
2018-02-22 00:05:26 -0500

    xarchiver: add missing runtime dependencies
    xarchiver uses a number of command-line tools to create and extract archives. This changeset adds those dependencies.
    
    NOTE: unarj is not included because it failed to build. And the rpm and dep dependencies are not included because I could not figure out what they should be.
    
    Closes #34933

--- pkgs/tools/archivers/xarchiver/default.nix
+++ pkgs/tools/archivers/xarchiver/default.nix
@@ -1,4 +1,5 @@
-{ stdenv, fetchFromGitHub, gtk3, pkgconfig, intltool, libxslt }:
+{ stdenv, fetchFromGitHub, gtk3, pkgconfig, intltool, libxslt, makeWrapper, 
+  coreutils, zip, unzip, p7zip, unrar, gnutar, bzip2, gzip, lhasa }:
 
 stdenv.mkDerivation rec {
   version = "0.5.4.12";
@@ -11,8 +12,13 @@ stdenv.mkDerivation rec {
     sha256 = "13d8slcx3frz0dhl1w4llj7001n57cjjb8r7dlaw5qacaas3xfwi";
   };
 
-  nativeBuildInputs = [ pkgconfig ];
+  nativeBuildInputs = [ pkgconfig makeWrapper ];
   buildInputs = [ gtk3 intltool libxslt ];
+
+  postFixup = ''
+    wrapProgram $out/bin/xarchiver \
+    --prefix PATH : ${stdenv.lib.makeBinPath [ zip unzip p7zip unrar gnutar bzip2 gzip lhasa coreutils ]}
+  '';
 
   meta = {
     description = "GTK+ frontend to 7z,zip,rar,tar,bzip2, gzip,arj, lha, rpm and deb (open and extract only)";

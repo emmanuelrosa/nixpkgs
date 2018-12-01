0727279055b4cf7f524c4632414ce924eab40b4a
Emmanuel Rosa <goaway1000@gmail.com>
2018-12-01 20:16:33 +0700

    masterpassword: add .desktop file

--- pkgs/applications/misc/masterpassword/default.nix
+++ pkgs/applications/misc/masterpassword/default.nix
@@ -1,6 +1,14 @@
-{ stdenv, fetchurl, makeWrapper, writeScript, jre }:
+{ stdenv, fetchurl, makeWrapper, writeScript, makeDesktopItem, jre }:
 
-stdenv.mkDerivation rec {
+let
+  desktopItem = makeDesktopItem {
+    name = "masterpassword";
+    exec = "masterpassword";
+    icon = "masterpassword";
+    desktopName = "Master Password";
+    categories = "Security";
+  };
+in stdenv.mkDerivation rec {
   name = "masterpassword-${version}";
   version = "2.7";
 
@@ -9,14 +17,23 @@ stdenv.mkDerivation rec {
     sha256 = "126nmqg6nnx5fl0gks2an7jl8krmpqnac6jrcbwnnd296zqbpvhk";
   };
 
+  icon = fetchurl {
+    url = https://masterpassword.app/img/favicon.png;
+    sha256 = "0znvkdvpb8hpd6pndbbviz44fp6ksfl1snq7w0287yf36qw3lbpv";
+  };
+
   buildInputs = [ makeWrapper ];
 
   builder = writeScript "builder.sh" ''  
     source $stdenv/setup
     mkdir -p $out/bin
+    mkdir -p $out/share/icons/hicolor/32x32
 
     makeWrapper ${jre}/bin/java $out/bin/masterpassword \
       --add-flags "-jar $src" \
+
+    ln -s ${desktopItem}/share/applications $out/share/applications
+    ln -s ${icon} $out/share/icons/hicolor/32x32/masterpassword.png
   '';
 
   meta = with stdenv.lib; {

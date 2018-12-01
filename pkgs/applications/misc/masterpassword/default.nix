d81b9b6485426e2884eef268d8207f9b64d00c0b
Emmanuel Rosa <goaway1000@gmail.com>
2018-12-01 23:14:00 +0700

    masterpassword: wrap in FHS chroot

--- pkgs/applications/misc/masterpassword/default.nix
+++ pkgs/applications/misc/masterpassword/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, makeWrapper, writeScript, makeDesktopItem, jre }:
+{ stdenv, fetchurl, makeWrapper, writeScript, makeDesktopItem, buildFHSUserEnv, jre, xorg }:
 
 let
   desktopItem = makeDesktopItem {
@@ -8,40 +8,46 @@ let
     desktopName = "Master Password";
     categories = "Security";
   };
-in stdenv.mkDerivation rec {
-  name = "masterpassword-${version}";
-  version = "2.7";
 
-  src = fetchurl {
-    url = https://masterpassword.app/masterpassword-gui.jar;
-    sha256 = "126nmqg6nnx5fl0gks2an7jl8krmpqnac6jrcbwnnd296zqbpvhk";
-  };
+  package = stdenv.mkDerivation rec {
+    name = "masterpassword-${version}";
+    version = "2.7";
 
-  icon = fetchurl {
-    url = https://masterpassword.app/img/favicon.png;
-    sha256 = "0znvkdvpb8hpd6pndbbviz44fp6ksfl1snq7w0287yf36qw3lbpv";
-  };
+    src = fetchurl {
+      url = https://masterpassword.app/masterpassword-gui.jar;
+      sha256 = "126nmqg6nnx5fl0gks2an7jl8krmpqnac6jrcbwnnd296zqbpvhk";
+    };
 
-  buildInputs = [ makeWrapper ];
+    icon = fetchurl {
+      url = https://masterpassword.app/img/favicon.png;
+      sha256 = "0znvkdvpb8hpd6pndbbviz44fp6ksfl1snq7w0287yf36qw3lbpv";
+    };
 
-  builder = writeScript "builder.sh" ''  
-    source $stdenv/setup
-    mkdir -p $out/bin
-    mkdir -p $out/share/icons/hicolor/32x32
+    buildInputs = [ makeWrapper ];
 
-    makeWrapper ${jre}/bin/java $out/bin/masterpassword \
-      --add-flags "-jar $src" \
+    builder = writeScript "builder.sh" ''  
+      source $stdenv/setup
+      mkdir -p $out/bin
+      mkdir -p $out/share/icons/hicolor/32x32
 
-    ln -s ${desktopItem}/share/applications $out/share/applications
-    ln -s ${icon} $out/share/icons/hicolor/32x32/masterpassword.png
-  '';
+      makeWrapper ${jre}/bin/java $out/bin/masterpassword \
+        --add-flags "-jar $src" \
 
-  meta = with stdenv.lib; {
-    description = "A stateless password management solution.";
-    longDescription = "The platform-independent GUI client implemented in Java.";
-    homepage = https://masterpassword.app;
-    license = licenses.gpl3;
-    maintainers = with maintainers; [ emmanuelrosa ];
-    platforms = platforms.all;
-  };
-}
+      ln -s ${desktopItem}/share/applications $out/share/applications
+      ln -s ${icon} $out/share/icons/hicolor/32x32/masterpassword.png
+    '';
+
+    meta = with stdenv.lib; {
+      description = "A stateless password management solution.";
+      longDescription = "The platform-independent GUI client implemented in Java.";
+      homepage = https://masterpassword.app;
+      license = licenses.gpl3;
+      maintainers = with maintainers; [ emmanuelrosa ];
+      platforms = platforms.all;
+    };
+};
+in buildFHSUserEnv {
+    name = "masterpassword-fhs";
+    targetPkgs = pkgs: [ pkgs.xorg.libX11 ];
+    runScript = "${package}/bin/masterpassword";
+  } // { inherit (package) meta name; }
